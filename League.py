21f78bbac653cabe4747a0a5d62aad5326a3e511
Ori Avni <ori.avni.code@gmail.com>
2024-11-21 22:31:45 +0200

    finished the main file but the bool_game method in League class needs a fix

--- League.py
+++ League.py
@@ -17,13 +17,29 @@ class League:
 
 
     def bool_game(self,points1,group1,points2,group2):
+        """
+        updates the status of both teams depending on the result of the game
+        :param points1: points for the first team
+        :param group1: team 1
+        :param points2: points for the second team
+        :param group2: team 2
+        :return: true if the names of the teams are listed in the League
+        """
+        counter = 0
         for team in self.teams:
-            if group1 not in self.teams or group2 not in self.teams:
-                return False
-        group1.game(points1,points2)
-        group2.game(points2,points1)
-        return True
+            if team.get_name() != group1 or team.get_name() != group2:
+                counter+=1
+        if counter == len(self.teams):
+            return False
+        else:
+            group1.game(points1,points2)
+            group2.game(points2,points1)
+            return True
 
     def print(self):
+        """
+        prints the table of the League
+        :return:
+        """
         for team in self.teams:
             team.print_team()
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from League import League
 
+
 names = []
-while True: #User inputs the names for the league
+
+while True:    #User inputs the names for the league
     name = input("Enter names for the teams: |do you want to stop? y=yes")
     if name == "y":
         break
     names.append(name)
 
+my_league = League(names) # creates a new League object using the constructor
 
-my_league = League(names)
-my_league.print()
+while True:
+
+    Team1 = input("Enter the name of the first team to play:")
+    Team2 = input("Enter the name of the second team to play:")
+    points1 = int(input("Enter the amount of goals the first team scored:"))
+    points2 = int(input("Enter the amount of goals the second team scored:"))
+
+    result = my_league.bool_game(group1 = Team1,group2 = Team2,points1=points1,points2=points2)
+
+    if result:
+        print("game recorded!")
+
+    my_league.print()
+
+    condition = input("Do you want to stop? | y=yes")
+    if condition == "y":
+        break
